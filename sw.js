ec59fab27685aadc6c5db3be61d69470dc1b0fb6
SevikFrajer <sevicek@post.cz>
2026-04-11 01:31:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'kick-chat-v2';
+const ASSETS = ['./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (ASSETS.some(a => e.request.url.endsWith(a.replace('./','')))) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
